fd150a75399d3ef0dcc37844f7916d696c66e6ff
Giovanna Moeller <giovannagmoeller@gmail.com>
2024-05-29 18:22:05 -0300

    aula 6.3 - conectando camadas do VIP

--- Buzz/Scenes/NewsListDetails/NewsListDetailsViewController.swift
+++ Buzz/Scenes/NewsListDetails/NewsListDetailsViewController.swift
@@ -15,6 +15,17 @@ protocol NewsListDetailsDisplayLogic: AnyObject {
 class NewsListDetailsViewController: UIViewController {
 
     var articleId: Int
+    var interactor: NewsListDetailsBusinessLogic?
+    
+    private lazy var articleTitleLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textColor = .white
+        label.textAlignment = .center
+        label.font = .systemFont(ofSize: 22.0, weight: .bold)
+        label.numberOfLines = 0
+        return label
+    }()
     
     init(articleId: Int) {
         self.articleId = articleId
@@ -28,14 +39,42 @@ class NewsListDetailsViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = UIColor(named: "PrimaryColor")
+        setup()
+        fetchNewsById()
+        addSubviews()
+        setupConstraints()
+    }
+    
+    private func addSubviews() {
+        view.addSubview(articleTitleLabel)
+    }
+    
+    private func setupConstraints() {
+        NSLayoutConstraint.activate([
+            articleTitleLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            articleTitleLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16.0),
+            articleTitleLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16.0),
+        ])
+    }
+    
+    private func fetchNewsById() {
+        interactor?.loadNewsFromId(request: NewsListDetailsModel.FetchNewsDetails.Request(id: articleId))
+    }
+    
+    private func setup() {
+        let viewController = self
+        let interactor = NewsListDetailsInteractor()
+        let presenter = NewsListDetailsPresenter()
+        viewController.interactor = interactor
+        interactor.presenter = presenter
+        presenter.viewController = viewController
     }
 
 }
 
 extension NewsListDetailsViewController: NewsListDetailsDisplayLogic {
     func displayFetchedNews(viewModel: NewsListDetailsModel.FetchNewsDetails.ViewModel) {
-        print(viewModel.displayedArticle)
-
+        articleTitleLabel.text = viewModel.displayedArticle.title
     }
     
     func displayError(message: String) {
