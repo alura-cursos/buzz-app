d40afe0d90f6d0abb5e86ce03bb9fb31070803af
Giovanna Moeller <giovannagmoeller@gmail.com>
2024-05-29 17:17:58 -0300

    aula 3.2 - aplicando o modelo no Interactor

--- Buzz/Scenes/NewsList/NewsListViewController.swift
+++ Buzz/Scenes/NewsList/NewsListViewController.swift
@@ -14,7 +14,7 @@ class NewsListViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .red
-        interactor.loadNews()
+        interactor.loadNews(request: NewsListModel.FetchNews.Request())
     }
 
 
--- Buzz/Scenes/NewsList/NewsListnteractor.swift
+++ Buzz/Scenes/NewsList/NewsListnteractor.swift
@@ -1,11 +1,19 @@
 //
-//  NewsListDetailsInteractor.swift
+//  NewsListInteractor.swift
 //  Buzz
 //
 //  Created by Giovanna Moeller on 29/05/24.
 //
 
 import Foundation
+
+protocol NewsListBusinessLogic {
+    func loadNews(request: NewsListModel.FetchNews.Request)
+}
+
+protocol NewsListDataStore {
+    var articles: [Article] { get set }
+}
 
 class NewsListInteractor {
     private var worker: NewsAPIWorker = NewsAPIWorker(networkService: URLSessionNetworking())
@@ -15,14 +23,14 @@ class NewsListInteractor {
         self.worker = worker
     }
 
-    func loadNews() {
+    func loadNews(request: NewsListModel.FetchNews.Request) {
         worker.fetchNews { [weak self] result in
             guard let self else { return }
             DispatchQueue.main.async {
                 switch result {
                 case .success(let fetchedArticles):
                     self.articles = fetchedArticles
-                    print(fetchedArticles)
+                    let response = NewsListModel.FetchNews.Response(articles: fetchedArticles)
                 case .failure(let failure):
                     print("Ocorreu um erro ao obter a lista de notícias: \(failure.localizedDescription)")
                 }
